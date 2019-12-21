e07f09c504a669e7df4722579893222cbaf43f95
Allison Zadrozny <allison@zadrozny.com>
2019-12-20 20:03:24 -0600

    Add test modal component for photos zoom-in.

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,6 @@ $( document ).ready(function() {
     centerMode: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    // autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 2000
   });
 });
